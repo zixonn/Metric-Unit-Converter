df5d26810c878227177b742ff71560c7e635608f
Zixon77 <chikaosro@gmail.com>
2024-09-11 18:06:38 -0400

    More config

--- App.js
+++ App.js
@@ -1,20 +1,25 @@
 import { StyleSheet, Text, View, Button} from 'react-native';
 import { useCounterStore, usePersistedCounterStore } from './store';
+import { Chip, PaperProvider } from 'react-native-paper';
 
 export default function App() {
+
   const {count, increment, decrement, setZero } = useCounterStore();
   const {persistedCount, persistedIncrement, persistedDecrement, persistedSetZero } = usePersistedCounterStore();
+
   return (
-    <View style={styles.container}>
-      <Text>{count}</Text>
-      <Button title = "Increment" onPress = {increment}/>
-      <Button title = "Decrement" onPress = {decrement}/>
-      <Button title = "Reset" onPress = {setZero}/>
-      <Text>{persistedCount}</Text>
-      <Button title = "Persistent Increment" onPress = {persistedIncrement}/>
-      <Button title = "Persistent Decrement" onPress = {persistedDecrement}/>
-      <Button title = "Persistent Reset" onPress = {persistedSetZero}/>
-    </View>
+    <PaperProvider>
+      <View style={styles.container}>
+        <Text>{count}</Text>
+        <Button title = "Increment" onPress = {increment}/>
+        <Button title = "Decrement" onPress = {decrement}/>
+        <Button title = "Reset" onPress = {setZero}/>
+        <Text>{persistedCount}</Text>
+        <Button title = "Persistent Increment" onPress = {persistedIncrement}/>
+        <Button title = "Persistent Decrement" onPress = {persistedDecrement}/>
+        <Button title = "Persistent Reset" onPress = {persistedSetZero}/>
+      </View>
+    </PaperProvider>
   );
 }
 
